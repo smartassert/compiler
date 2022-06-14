8738f174820bce5555e9a10b2f84002e19b571a0
Jon Cram <webignition@gmail.com>
2022-06-14 17:23:57 +0100

    Add configuration validation tests

--- tests/Unit/Command/GenerateCommandTest.php
+++ tests/Unit/Command/GenerateCommandTest.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace SmartAssert\Compiler\Tests\Unit\Command;
 
+use phpmock\mockery\PHPMockery;
 use SmartAssert\Compiler\Command\GenerateCommand;
+use SmartAssert\Compiler\Model\Options;
 use SmartAssert\Compiler\Services\Compiler;
 use SmartAssert\Compiler\Services\ConfigurationFactory;
 use SmartAssert\Compiler\Services\ErrorOutputFactory;
@@ -50,5 +52,194 @@ class GenerateCommandTest extends AbstractBaseTest
 
         $commandOutput = ErrorOutput::fromArray((array) Yaml::parse($stderr->fetch()));
         self::assertEquals($expectedCommandOutput, $commandOutput);
+    }
+
+    /**
+     * @dataProvider runInvalidConfigurationDataProvider
+     *
+     * @param array<mixed> $input
+     * @param array<mixed> $expectedOutputData
+     */
+    public function testRunInvalidConfiguration(
+        array $input,
+        callable $initializer,
+        int $expectedExitCode,
+        array $expectedOutputData,
+    ): void {
+        $initializer();
+
+        $stderr = new BufferedOutput();
+
+        $command = new GenerateCommand(
+            \Mockery::mock(TestLoader::class),
+            \Mockery::mock(Compiler::class),
+            \Mockery::mock(TestWriter::class),
+            new ErrorOutputFactory(new ValidatorInvalidResultSerializer()),
+            new OutputRenderer(\Mockery::mock(OutputInterface::class), $stderr),
+            new ConfigurationFactory()
+        );
+
+        $exitCode = $command->run(new ArrayInput($input), new NullOutput());
+
+        self::assertSame($expectedExitCode, $exitCode);
+
+        $output = $stderr->fetch();
+        $outputData = Yaml::parse($output);
+        self::assertIsArray($outputData);
+
+        self::assertSame($expectedOutputData, $outputData);
+    }
+
+    /**
+     * @return array<mixed>
+     */
+    public function runInvalidConfigurationDataProvider(): array
+    {
+        $mockNamespace = 'webignition\BasilCompilerModels';
+        $isReadableMockArguments = [$mockNamespace, 'is_readable'];
+        $isDirMockArguments = [$mockNamespace, 'is_dir'];
+        $isWritableMockArguments = [$mockNamespace, 'is_writable'];
+
+        $sourcePath = getcwd() . '/tests/Fixtures/basil/Test/example.com.verify-open-literal.yml';
+        $targetPath = getcwd() . '/tests/Fixtures/target';
+
+        return [
+            'source empty' => [
+                'input' => [
+                    '--' . Options::OPTION_SOURCE => '',
+                    '--' . Options::OPTION_TARGET => $targetPath,
+                    '--' . Options::OPTION_BASE_CLASS => AbstractBaseTest::class,
+                ],
+                'initializer' => function () {
+                },
+                'expectedExitCode' => ErrorOutputFactory::CODE_COMMAND_CONFIG_SOURCE_EMPTY,
+                'expectedErrorData' => [
+                    'message' => ErrorOutputFactory::MESSAGE_COMMAND_CONFIG_SOURCE_EMPTY,
+                    'code' => ErrorOutputFactory::CODE_COMMAND_CONFIG_SOURCE_EMPTY,
+                ],
+            ],
+            'source not absolute' => [
+                'input' => [
+                    '--' . Options::OPTION_SOURCE => 'relative.yaml',
+                    '--' . Options::OPTION_TARGET => $targetPath,
+                    '--' . Options::OPTION_BASE_CLASS => AbstractBaseTest::class,
+                ],
+                'initializer' => function () {
+                },
+                'expectedExitCode' => ErrorOutputFactory::CODE_COMMAND_CONFIG_SOURCE_NOT_ABSOLUTE,
+                'expectedErrorData' => [
+                    'message' => ErrorOutputFactory::MESSAGE_COMMAND_CONFIG_SOURCE_NOT_ABSOLUTE,
+                    'code' => ErrorOutputFactory::CODE_COMMAND_CONFIG_SOURCE_NOT_ABSOLUTE,
+                ],
+            ],
+            'source not readable' => [
+                'input' => [
+                    '--' . Options::OPTION_SOURCE => $sourcePath,
+                    '--' . Options::OPTION_TARGET => $targetPath,
+                    '--' . Options::OPTION_BASE_CLASS => AbstractBaseTest::class,
+                ],
+                'initializer' => function () use ($isReadableMockArguments, $sourcePath) {
+                    PHPMockery::mock(...$isReadableMockArguments)
+                        ->with($sourcePath)
+                        ->andReturnFalse()
+                    ;
+                },
+                'expectedExitCode' => ErrorOutputFactory::CODE_COMMAND_CONFIG_SOURCE_NOT_READABLE,
+                'expectedErrorData' => [
+                    'message' => ErrorOutputFactory::MESSAGE_COMMAND_CONFIG_SOURCE_NOT_READABLE,
+                    'code' => ErrorOutputFactory::CODE_COMMAND_CONFIG_SOURCE_NOT_READABLE,
+                ],
+            ],
+            'target empty' => [
+                'input' => [
+                    '--' . Options::OPTION_SOURCE => $sourcePath,
+                    '--' . Options::OPTION_TARGET => '',
+                    '--' . Options::OPTION_BASE_CLASS => AbstractBaseTest::class,
+                ],
+                'initializer' => function () {
+                },
+                'expectedExitCode' => ErrorOutputFactory::CODE_COMMAND_CONFIG_TARGET_EMPTY,
+                'expectedErrorData' => [
+                    'message' => ErrorOutputFactory::MESSAGE_COMMAND_CONFIG_TARGET_EMPTY,
+                    'code' => ErrorOutputFactory::CODE_COMMAND_CONFIG_TARGET_EMPTY,
+                ],
+            ],
+            'target not absolute' => [
+                'input' => [
+                    '--' . Options::OPTION_SOURCE => $sourcePath,
+                    '--' . Options::OPTION_TARGET => 'relative',
+                    '--' . Options::OPTION_BASE_CLASS => AbstractBaseTest::class,
+                ],
+                'initializer' => function () {
+                },
+                'expectedExitCode' => ErrorOutputFactory::CODE_COMMAND_CONFIG_TARGET_NOT_ABSOLUTE,
+                'expectedErrorData' => [
+                    'message' => ErrorOutputFactory::MESSAGE_COMMAND_CONFIG_TARGET_NOT_ABSOLUTE,
+                    'code' => ErrorOutputFactory::CODE_COMMAND_CONFIG_TARGET_NOT_ABSOLUTE,
+                ],
+            ],
+            'target not directory' => [
+                'input' => [
+                    '--' . Options::OPTION_SOURCE => $sourcePath,
+                    '--' . Options::OPTION_TARGET => $targetPath,
+                    '--' . Options::OPTION_BASE_CLASS => AbstractBaseTest::class,
+                ],
+                'initializer' => function () use (
+                    $isReadableMockArguments,
+                    $isDirMockArguments,
+                    $sourcePath,
+                    $targetPath
+                ) {
+                    PHPMockery::mock(...$isReadableMockArguments)
+                        ->with($sourcePath)
+                        ->andReturnTrue()
+                    ;
+
+                    PHPMockery::mock(...$isDirMockArguments)
+                        ->with($targetPath)
+                        ->andReturnFalse()
+                    ;
+                },
+                'expectedExitCode' => ErrorOutputFactory::CODE_COMMAND_CONFIG_TARGET_NOT_A_DIRECTORY,
+                'expectedErrorData' => [
+                    'message' => ErrorOutputFactory::MESSAGE_COMMAND_CONFIG_TARGET_NOT_A_DIRECTORY,
+                    'code' => ErrorOutputFactory::CODE_COMMAND_CONFIG_TARGET_NOT_A_DIRECTORY,
+                ],
+            ],
+            'target not writable' => [
+                'input' => [
+                    '--' . Options::OPTION_SOURCE => $sourcePath,
+                    '--' . Options::OPTION_TARGET => $targetPath,
+                    '--' . Options::OPTION_BASE_CLASS => AbstractBaseTest::class,
+                ],
+                'initializer' => function () use (
+                    $isReadableMockArguments,
+                    $isDirMockArguments,
+                    $isWritableMockArguments,
+                    $sourcePath,
+                    $targetPath
+                ) {
+                    PHPMockery::mock(...$isReadableMockArguments)
+                        ->with($sourcePath)
+                        ->andReturnTrue()
+                    ;
+
+                    PHPMockery::mock(...$isDirMockArguments)
+                        ->with($targetPath)
+                        ->andReturnTrue()
+                    ;
+
+                    PHPMockery::mock(...$isWritableMockArguments)
+                        ->with($targetPath)
+                        ->andReturnFalse()
+                    ;
+                },
+                'expectedExitCode' => ErrorOutputFactory::CODE_COMMAND_CONFIG_TARGET_NOT_WRITABLE,
+                'expectedErrorData' => [
+                    'message' => ErrorOutputFactory::MESSAGE_COMMAND_CONFIG_TARGET_NOT_WRITABLE,
+                    'code' => ErrorOutputFactory::CODE_COMMAND_CONFIG_TARGET_NOT_WRITABLE,
+                ],
+            ],
+        ];
     }
 }
